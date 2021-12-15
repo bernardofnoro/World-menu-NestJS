9650365a2d7305fc8f63d0627939eb2a6b48a6ca
chanelym <chanelym@gmail.com>
2021-12-14 22:45:55 -0300

    (fix): JWT Authentication

--- backend/food-atlas/src/main.ts
+++ backend/food-atlas/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('World Menu')
     .setDescription('REST API')
